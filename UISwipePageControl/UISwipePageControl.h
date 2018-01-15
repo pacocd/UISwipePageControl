37b4cc2ffb704a8991bf3959b8b86e1a509b897e
Paco Chacon <pacochacon94@gmail.com>
2018-01-15 01:15:00 -0600

    Added XCode project files

---
+++ UISwipePageControl/UISwipePageControl.h
@@ -0,0 +1,19 @@
+//
+//  UISwipePageControl.h
+//  UISwipePageControl
+//
+//  Created by Francisco Javier Chacon de Dios on 15/01/18.
+//  Copyright © 2018 Francisco Javier Chacon de Dios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UISwipePageControl.
+FOUNDATION_EXPORT double UISwipePageControlVersionNumber;
+
+//! Project version string for UISwipePageControl.
+FOUNDATION_EXPORT const unsigned char UISwipePageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UISwipePageControl/PublicHeader.h>
+
+
